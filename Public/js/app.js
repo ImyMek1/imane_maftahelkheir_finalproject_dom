0c241234a48c8bb9ea4562d25fbd937cfdf56597
imane.maftah19@gmail.com <imane.maftah19@gmail.com>
2026-01-20 22:58:40 +0100

    Stats Section and tab version

--- Public/js/app.js
+++ Public/js/app.js
@@ -24,3 +24,34 @@ window.addEventListener("scroll", () => {
 });
 
 ///////////////////
+let counters = document.querySelectorAll(".stat-box h3");
+let statsStarted = false;
+
+window.addEventListener("scroll", () => {
+  let statsSection = document.querySelector(".stats");
+  let position = statsSection.getBoundingClientRect().top;
+  let screenHeight = window.innerHeight;
+
+  if (position < screenHeight && !statsStarted) {
+    counters.forEach(counter => {
+      let target = +counter.dataset.count;
+      let count = 0;
+
+      let increment = target / 100;
+
+      let updateCount = () => {
+        if (count < target) {
+          count += increment;
+          counter.innerText = Math.ceil(count);
+          setTimeout(updateCount, 20);
+        } else {
+          counter.innerText = target;
+        }
+      };
+
+      updateCount();
+    });
+
+    statsStarted = true;
+  }
+});
