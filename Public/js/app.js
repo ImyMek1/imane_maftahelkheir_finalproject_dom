dea5e069ed4479bf4de08a8be6d710f0f183083f
imane.maftah19@gmail.com <imane.maftah19@gmail.com>
2026-01-20 22:25:54 +0100

    Why Section web and tab version

--- Public/js/app.js
+++ Public/js/app.js
@@ -10,3 +10,17 @@ window.addEventListener("load", () => {
 });
 
 ///////////////////
+
+///////////////////
+let whySection = document.querySelector(".why-us");
+
+window.addEventListener("scroll", () => {
+  let pos = whySection.getBoundingClientRect().top;
+  let screen = window.innerHeight;
+
+  if (pos < screen - 150) {
+    whySection.classList.add("show");
+  }
+});
+
+///////////////////
